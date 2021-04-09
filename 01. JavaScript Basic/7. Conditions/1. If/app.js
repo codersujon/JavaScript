1c6e437cb7780d485a2bc6079ad23f0cf6060039
Md.Sujon Ahmed <shobuj2sujon@gmail.com>
2021-04-10 05:23:36 +0600

    If Condition

---
+++ 01. JavaScript Basic/7. Conditions/1. If/app.js
@@ -0,0 +1,32 @@
+
+
+/*
+ 1:
+ ================================
+ var age = prompt("Enter your age");
+
+	if (age >= 50) {
+		console.log("Old");
+	} else {
+		console.log("Under 50");
+	}
+
+*/
+
+/*
+ 2:
+ ================================
+	var myAge = prompt("Enter your age:");
+	if(myAge >= 18){
+	  console.log("You are adult");
+	}
+
+	if(myAge < 18){
+	  console.log("You are not adult");
+	}
+
+	if(myAge > 20 && myAge < 60){
+	  console.log("You are a young person");
+	}
+
+*/
