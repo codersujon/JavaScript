e0b7c0a449c5237cba7bf9852f2a92fdd09fccac
Md.Sujon Ahmed <shobuj2sujon@gmail.com>
2021-04-12 03:07:37 +0600

    Continue and Break Loop

---
+++ 01. JavaScript Basic/8. Loops & Loop Control/Loop Control/2. Break loop/app.js
@@ -0,0 +1,15 @@
+/*
+ 1:
+ ================================
+	for(var i = 0; i < 3; i++){
+		for(var j = 0; j <3; j++){
+			document.write("This is i:", i + "<br>");
+			document.write(j);
+			if (j == 1) {
+				break;
+			}
+		}
+	}	
+*/
+
+
