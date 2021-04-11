ad0cb8cf76cdb6aceeaca19e82b3a89fea679e3d
Md.Sujon Ahmed <shobuj2sujon@gmail.com>
2021-04-12 03:26:31 +0600

    For in Loop

---
+++ 01. JavaScript Basic/8. Loops & Loop Control/1. Loops/4. For in Loop/app.js
@@ -0,0 +1,23 @@
+//For in Loop:
+	// String / array / Object
+
+/*
+ 1:
+ ================================
+	
+*/
+
+var name = "Md.Sujon Ahmed";
+
+for(var x in name){
+	document.write(name[x], "<br>");
+}
+
+
+
+var mobileBrand = ['Nokia', 'Itel','Samsung','Iphone'];
+var person = {
+	name: 'Md.Sujon Ahmed',
+	age: 24,
+	country: 'Dhaka, Bangladesh'
+}
