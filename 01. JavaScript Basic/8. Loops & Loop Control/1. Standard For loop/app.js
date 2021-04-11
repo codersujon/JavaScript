6078119d435c022da042077f37a7605cce8d9218
Md.Sujon Ahmed <shobuj2sujon@gmail.com>
2021-04-12 02:00:41 +0600

    Loops: Standard For Loop

---
+++ 01. JavaScript Basic/8. Loops & Loop Control/1. Standard For loop/app.js
@@ -0,0 +1,31 @@
+/*
+ 1:
+ ================================
+	//Write a program to write your name 10 times.
+	for( var i = 0; i < 10; i++){
+		document.write("Md.Sujon Ahmed" + "<br/>");
+	}
+*/
+
+/*
+ 2:
+ ================================
+	//Write a program to print 0 to 100.
+	for(var i = 0; i<=100; i++){
+		document.write(i + "<br/>");
+	}
+*/
+
+/*
+ 3:
+ ================================
+	var fruits = ['apple','Orange','Jackfruit','Watermelon'];
+
+	for(var i = 0; i < fruits.length; i++){
+		document.write(fruits[i], "<br/>");
+	}
+*/
+
+for(var i = 100; i >= 0; i--){
+	document.write(i, "<br/>");
+}
