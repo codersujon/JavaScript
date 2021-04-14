01816fd8690d5e946e8624c9ca608a16ad8e1f03
Md.Sujon Ahmed <shobuj2sujon@gmail.com>
2021-04-15 01:41:03 +0600

    Math Object- all Math method

---
+++ 02. Advance JavaScript/7. Math Object/app.js
@@ -0,0 +1,31 @@
+//Math Exponent n^n
+console.log(Math.E);
+//Math PI Number value
+console.log(Math.PI);
+
+
+//Absulate Number mane ja ase tai
+var n = 4.49;
+console.log(Math.abs(n));
+
+//Floor method jer mane dibe tar nicher valo print krbe
+console.log(Math.floor(n));
+
+//Ceil method that means upper value print krbe
+console.log(Math.ceil(n));
+
+//Round method
+console.log(Math.round(n));
+
+//Maximum and Minumum
+console.log(Math.max(100,650,1000));
+console.log(Math.min(50, 2, 500, 300));
+
+// Power of number
+console.log(Math.pow(2, 16));
+
+//Square root
+console.log(Math.sqrt(9));
+
+//Random Number ****
+console.log(Math.round(Math.random() * 6500));
