044ebaf7cf7c54a7bc7e3875114907ca4b51a9cf
Md.Sujon Ahmed <shobuj2sujon@gmail.com>
2021-04-14 07:29:37 +0600

    Iteration with map

---
+++ 02. Advance JavaScript/5. Array Iteration Using map/app.js
@@ -0,0 +1,28 @@
+/**
+ * Array Iteration Using Map
+ */
+
+
+var foods = ['Cheese','Egg','Butter','Margarine','Yogurt','Ice cream','Cream'];
+
+/*function addSomething(item){
+	return (`${item} is a food`);
+}
+
+let result = foods.map(addSomething);
+console.log(result);*/
+
+/*var number = [1, 50, 21];
+
+var squareNumber = number.map(function(item){
+	return item*item;
+});
+
+console.log(squareNumber);*/
+
+/*let foodItem = foods.map(function(item, i, abc){
+	return `${item, i, abc} is a good food`;
+});
+console.log(foodItem);*/
+
+
