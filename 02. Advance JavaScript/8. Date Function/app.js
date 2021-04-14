244f16cf6c38ec212d686225a950527a8f1b41ff
Md.Sujon Ahmed <shobuj2sujon@gmail.com>
2021-04-15 02:01:56 +0600

    Date Function- Most important

---
+++ 02. Advance JavaScript/8. Date Function/app.js
@@ -0,0 +1,29 @@
+//Date Function or Method
+// javaScript Built in Function new Date();
+let date = new Date();
+console.log(date);
+console.log(date.toString());
+//to print local date and time  as like we see
+console.log(date.toLocaleString());
+
+console.log(date.toTimeString());
+
+//Show Full Year
+console.log(date.getFullYear());
+
+//Show Full Month
+console.log(date.getMonth());
+
+//Date
+console.log(date.getDate());
+//Hourse
+console.log(date.getHours());
+//Minutes
+console.log(date.getMinutes());
+//Seconds
+console.log(date.getSeconds());
+//Milli Seconds
+console.log(date.getMilliseconds());
+
+//Day
+console.log(date.getDay());
