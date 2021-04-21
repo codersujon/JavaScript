e08c54452d8b8eaf901639a2283464e40fdd75d1
Md.Sujon Ahmed <shobuj2sujon@gmail.com>
2021-04-21 21:40:04 +0600

    QuerySelectorAll-Important

--- 03. Advanced JavaScript/6. Multiple Element Selector/app.js
+++ 03. Advanced JavaScript/6. Multiple Element Selector/app.js
@@ -16,3 +16,31 @@ list = document.querySelector('ul').getElementsByTagName('li');
 list[0].style.background = 'yellow';
 console.log(list[0]);
 
+
+
+//document.querySelectorAll()****
+// id -> #
+// class -> .
+// tag -> tag name
+
+let list1 = document.querySelectorAll('.sample-class');
+	list1 = document.querySelectorAll('ul li');
+
+	list1.forEach(function(item){
+		//console.log(item)
+	});
+
+let liOdd = document.querySelectorAll('li:nth-child(odd)');
+liOdd.forEach(function(item){
+	item.style.background = 'orange';
+	item.style.color = 'white';
+});
+
+let lieven = document.querySelectorAll('li:nth-child(even)');
+
+lieven.forEach(function(item){
+	item.style.background = 'green';
+	item.style.color = '#fff';
+	item.style.padding = '10px';
+});
+console.log(liOdd);
