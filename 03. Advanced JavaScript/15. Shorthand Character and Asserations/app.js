3a661ab6881cb9e0c2a67c61f8d4aeaa89551f83
Md.Sujon Ahmed <shobuj2sujon@gmail.com>
2021-05-01 15:56:37 +0600

    Shorthand Character

---
+++ 03. Advanced JavaScript/15. Shorthand Character and Asserations/app.js
@@ -0,0 +1,46 @@
+/**
+ * Regular Expression
+ * ============== Shorthand Character and Asserations
+ */
+
+
+
+let re;
+ re  = /\w/; // Word Character means Alpha, Numeric or Underscore
+ re = /\w+/; // One or more show krbe
+ re = /\W/; // Non Word Character
+ re = /\W+/; // Non Word Character one or More
+ re = /\d/; // Digit
+ re = /\d+/; // One or More Digit show
+ re = /\D/; // Non Digit Characters
+ re = /\s/; // Only WhiteSpace
+ re = /\S/; 
+ re = /\bhello\b/; // word boundary only space working first and end e
+
+
+let str;
+str = "+01716259641";
+str = "(*^_dfdsfa";
+str = "(*^_dfdsfa";
+str = "dfdfa(";
+str = "dfdfa(dfd^";
+str = "ass5";
+str = "asdfd56dfds5";
+str = "5455";
+str = "hello world";
+str = " hello ";
+str = "hello";
+
+console.log(re.exec(str));
+
+
+
+
+function reTest(re, str){
+	if(re.test(str)){
+		console.log(`${str} Matches ${re.source}`);
+	}else{
+		console.log(`${str} doesn't match ${re.source}`);
+	}
+}
+reTest(re, str);
